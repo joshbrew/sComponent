009ddd31e8d126e9e7851d3a395e48e41d2ef8db
Joshua Brewster (Bubba Skrimp) <brewster.joshua1@gmail.com>
2025-05-19 15:13:49 -0700

    fixes for async behavior, callback enabling

--- dist/state.component.d.ts
+++ dist/state.component.d.ts
@@ -1,25 +1,37 @@
 import { Component } from 'react';
 import { EventHandler } from './EventHandler';
 export declare const state: EventHandler;
-export declare class sComponent<P = {} & {
+export declare class sComponent<P extends {
     state?: EventHandler;
     doNotSubscribe?: string[];
-}, S = {}> extends Component<P, S> {
+} = {}, S extends Record<string, any> = {}> extends Component<P, S> {
     state: any;
     __statemgr: EventHandler;
-    __state_subs: {
-        [key: string]: number;
-    };
-    __updated: any[];
+    __state_subs: Record<string, number>;
+    __updated: string[];
     __unique: string;
-    react_setState: any;
-    setState: (s: any) => void;
+    /**
+     * Promise-based setState that relays once to your EventHandler
+     */
+    setState(partialState: Partial<S>, callback?: () => void): Promise<void>;
     constructor(props?: P & {
         state?: EventHandler;
         doNotSubscribe?: string[];
     });
+    /**
+     * Subscribe to a single state property
+     */
     __subscribeComponent(prop: string, onEvent?: (value: any) => void): number;
+    /**
+     * Unsubscribe from one or all properties
+     */
     __unsubscribeComponent(prop?: string): void;
-    __setUseLocalStorage(bool: boolean): void;
+    /**
+     * Toggle localStorage syncing at runtime
+     */
+    __setUseLocalStorage(use: boolean): void;
+    /**
+     * Restore persisted values into this.state
+     */
     __restoreLocalStorage(): void;
 }
--- state.component.tsx
+++ state.component.tsx
@@ -1,106 +1,141 @@
 import React, { Component } from 'react';
 import { EventHandler } from './EventHandler';
 
-export const state = new EventHandler();
+export const state = new EventHandler(); // import this anywhere for direct manipulation of state components from script
 
-export class sComponent<P = {} & {
-    state?: EventHandler;
-    doNotSubscribe?: string[]; // can skip certain props
-}, S = {}> extends Component<P, S> {
+export class sComponent<
+    P extends { state?: EventHandler; doNotSubscribe?: string[] } = {},
+    S extends Record<string, any> = {}
+> extends Component<P, S> {
+    // React-managed state
+    state = {} as any;
 
-    state={} as any;
+    // Global or injected EventHandler
+    __statemgr: EventHandler;
 
-    __statemgr = state as EventHandler;
-    __state_subs: { [key: string]: number } = {};
-    __updated: any[] = [];
-    __unique = `component${Math.floor(Math.random() * 1000000000000000)}`;
+    // Subscription tokens
+    __state_subs: Record<string, number> = {};
 
+    // Keys just updated by setState (to suppress echoes)
+    __updated: string[] = [];
+
+    // Unique identifier for debugging or DOM-keying
+    __unique = `component${Math.floor(Math.random() * 1e15)}`;
+
+    /**
+     * Promise-based setState that relays once to your EventHandler
+     */
     //@ts-ignore
-    react_setState = this.setState.bind(this);
-     
-    setState = (s: any) => {
-        this.__updated = Object.keys(s);
-        this.react_setState(s);
-        if (typeof s === 'object') {
-            this.__statemgr.setState(s); // now relay through event handler
-        }
+    setState(
+        partialState: Partial<S>,
+        callback?: () => void
+    ): Promise<void> {
+        return new Promise(resolve => {
+            super.setState(partialState as any, () => {
+                // record which keys were updated
+                this.__updated = Object.keys(partialState);
+                // relay the entire object to EventHandler in one call
+                this.__statemgr.setState(partialState);
+                // user callback
+                if (callback) callback();
+                resolve();
+            });
+        });
     }
 
     constructor(
-        props: P & {
-            state?: EventHandler;
-            doNotSubscribe?: string[]; // can skip certain props
-        } = {} as P & {
-            state?: EventHandler; // can apply a new state other than the global state so you can have states for certain pages, for example
-        }
+        props: P & { state?: EventHandler; doNotSubscribe?: string[] } = {} as P
     ) {
         super(props);
+        // use injected or global state manager
+        this.__statemgr = props.state ?? state;
 
-        if (props.state) // synced with global state
-            this.__statemgr = props.state;
-
-
-        let found: Partial<S> = {};
-        for (const prop in this.state) { // for all props in state, subscribe to changes in the global state
-            if (props?.doNotSubscribe && props.doNotSubscribe.indexOf(prop) > -1) continue;
-            if (prop in this.__statemgr.data) found[prop as keyof S] = this.__statemgr.data[prop];
+        // ── Hydrate initial state from EventHandler.data
+        const initial: Partial<S> = {};
+        for (const prop in this.state) {
+            if (props.doNotSubscribe?.includes(prop)) continue;
+            if (prop in this.__statemgr.data) {
+                initial[prop as keyof S] = this.__statemgr.data[prop];
+            }
         }
-        if (Object.keys(found).length > 0) {
-            Object.assign(this.state, found);
+        if (Object.keys(initial).length) {
+            Object.assign(this.state, initial);
         }
 
-        if(this.__statemgr.useLocalStorage) {
+        // ── Hydrate from localStorage if enabled
+        if (this.__statemgr.useLocalStorage) {
             this.__restoreLocalStorage();
         }
-        
+
+        // ── Defer subscription & final override to next tick
         setTimeout(() => {
-            for (const prop in this.state) { // for all props in state, subscribe to changes in the global state
-                if (props?.doNotSubscribe && props.doNotSubscribe.indexOf(prop) > -1) continue;
-                if (prop in this.__statemgr.data) found[prop as keyof S] = this.__statemgr.data[prop];
+            const override: Partial<S> = {};
+            for (const prop in this.state) {
+                if (props.doNotSubscribe?.includes(prop)) continue;
+                if (prop in this.__statemgr.data) {
+                    override[prop as keyof S] = this.__statemgr.data[prop];
+                }
                 this.__subscribeComponent(prop);
             }
-            if (Object.keys(found).length > 0) this.react_setState(found); // override defaults
-        }, 0.001);
-    }
-
-    __subscribeComponent(prop: string, onEvent?: (value: any) => void) {
-        let sub = this.__statemgr.subscribeEvent(prop, (res) => {
-            let c = this;
-            if (typeof c === 'undefined') { // the class will be garbage collected by react and this will work to unsubscribe
-                this.__statemgr.unsubscribeEvent(prop, sub);
-            } else {
-                if (onEvent) onEvent(res);
-                let wasupdated = this.__updated.indexOf(prop);
-                if (wasupdated > -1) {
-                    this.__updated.splice(wasupdated, 1);
-                } else {
-                    this.react_setState({ [prop]: res } as Pick<S, keyof S>); // only updates one prop at a time rn
-                }
+            if (Object.keys(override).length) {
+                super.setState(override as any);
             }
-        });
-        this.__state_subs[prop] = sub as number;
-
-        return sub;
+        }, 0);
     }
 
+    /**
+     * Subscribe to a single state property
+     */
+    __subscribeComponent(
+        prop: string,
+        onEvent?: (value: any) => void
+    ) {
+        const token = this.__statemgr.subscribeEvent(prop, (res) => {
+            if (onEvent) onEvent(res);
+            const idx = this.__updated.indexOf(prop);
+            if (idx > -1) {
+                // this update came from us, suppress echo
+                this.__updated.splice(idx, 1);
+            } else {
+                super.setState({ [prop]: res } as Pick<S, keyof S>);
+            }
+        }) as number;
+
+        this.__state_subs[prop] = token;
+        return token;
+    }
+
+    /**
+     * Unsubscribe from one or all properties
+     */
     __unsubscribeComponent(prop?: string) {
-        if (!prop) {
+        if (prop) {
+            this.__statemgr.unsubscribeEvent(prop, this.__state_subs[prop]);
+        } else {
             for (const key in this.__state_subs) {
                 this.__statemgr.unsubscribeEvent(key, this.__state_subs[key]);
             }
-        } else this.__statemgr.unsubscribeEvent(prop, this.__state_subs[prop]);
+            this.__state_subs = {};
+        }
     }
 
-    //should only have to run this on a parent component then the others will restore their state.
-    __setUseLocalStorage(bool:boolean) {
-        this.__statemgr.useLocalStorage = bool;
+    /**
+     * Toggle localStorage syncing at runtime
+     */
+    __setUseLocalStorage(use: boolean) {
+        this.__statemgr.useLocalStorage = use;
         this.__restoreLocalStorage();
-    }   
+    }
 
+    /**
+     * Restore persisted values into this.state
+     */
     __restoreLocalStorage() {
-        let result = this.__statemgr.restoreLocalStorage(Object.keys(this.state));
-        if(result) {
-            Object.assign(this.state,result);
+        const result = this.__statemgr.restoreLocalStorage(
+            Object.keys(this.state)
+        );
+        if (result) {
+            Object.assign(this.state, result);
         }
     }
 }
